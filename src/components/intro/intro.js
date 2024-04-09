788f585dc607e957815064c46ec98492a4c1e04b
Mathieu <mathieuvantuil2000@gmail.com>
2024-04-09 12:44:47 +0200

    Changed some files and changed a few projects

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,22 +1,25 @@
 import React from "react";
-import './intro.css';
-import bg from '../../assets/image.png';
-import btnImg from '../../assets/hireme.png';
-import { Link } from 'react-scroll';
+import "./intro.css";
+import bg from "../../assets/image.png";
 
 const Intro = () => {
-    return (
-        <section id="intro">
-            <div className="introContent">
-                <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Mathieu</span> <br/>Web Developer</span>
-                <p className="introPara">I am a skilled web developer with experince in creating <br/> beautifull and user-friendly websites.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire me" className="btnImg" />Hire me</button></Link>
-
-            </div>
-            <img src={bg} alt="Profile" className="bg"/>
-        </section>
-    )
-}
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello,</span>
+        <span className="introText">
+          I'm <span className="introName">Mathieu</span> <br />
+          Web Developer
+        </span>
+        <p className="introPara">
+          I am a skilled web developer with experince in creating <br />{" "}
+          beautifull and user-friendly websites.
+        </p>
+        {/* <Link><button className="btn"><img src={btnImg} alt="Hire me" className="btnImg" />Hire me</button></Link> */}
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
 
 export default Intro;
--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,28 +1,34 @@
 import React from "react";
-import './project.css';
-import portfolio1 from '../../assets/portfolio-1.png';
-import portfolio2 from '../../assets/portfolio-2.png'
-import portfolio3 from '../../assets/portfolio-3.png'
-import portfolio4 from '../../assets/portfolio-4.png'
-import portfolio5 from '../../assets/portfolio-5.png'
-import portfolio6 from '../../assets/portfolio-6.png'
+import "./project.css";
+import portfolio1 from "../../assets/portfolio-1.png";
+import portfolio2 from "../../assets/portfolio-2.png";
+import portfolio3 from "../../assets/portfolio-3.png";
+import portfolio4 from "../../assets/portfolio-4.png";
+import portfolio5 from "../../assets/portfolio-5.png";
+import portfolio6 from "../../assets/portfolio-6.png";
 
 const Project = () => {
-    return (
-        <section id="projects">
-            <h2 className="projectTitle">My finished projects</h2>
-            <span className="projectDesc">I do not have many projects done just now but there will be more ^w^</span>
-            <div className="projectImgs">
-                <img src={portfolio1} alt="" className="projectImg"/>
-                <img src={portfolio2} alt="" className="projectImg"/>
-                <img src={portfolio3} alt="" className="projectImg"/>
-                <img src={portfolio4} alt="" className="projectImg"/>
-                <img src={portfolio5} alt="" className="projectImg"/>
-                <img src={portfolio6} alt="" className="projectImg"/>
-            </div>
-            <button className="projectBtn">See more</button>
-        </section>
-    );
-}
+  return (
+    <section id="projects">
+      <h2 className="projectTitle">My finished projects</h2>
+      <span className="projectDesc">
+        I do not have many projects done just now but there will be more ^w^
+      </span>
+      <div className="projectImgs">
+        <img src={portfolio1} alt="" className="projectImg" />
+        <img src={portfolio2} alt="" className="projectImg" />
+        <img src={portfolio3} alt="" className="projectImg" />
+        <img src={portfolio4} alt="" className="projectImg" />
+        <img src={portfolio5} alt="" className="projectImg" />
+        <img src={portfolio6} alt="" className="projectImg" />
+      </div>
+      <button className="projectBtn">
+        <a target="blank" href="https://github.com/MrMatt04">
+          See more
+        </a>
+      </button>
+    </section>
+  );
+};
 
 export default Project;
